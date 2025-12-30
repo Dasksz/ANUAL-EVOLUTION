8c8c8cb735d15971df37f386ef821a1763693126
Daniel César <144414027+Dasksz@users.noreply.github.com>
2025-12-30 09:13:03 -0300

    Delete sql 2 de 2.sql

--- sql 2 de 2.sql
+++
@@ -1,7 +0,0 @@
--- =================================================================
--- SCRIPT V13.1-B (APENAS O ÍNDICE)
--- OBJETIVO: Criar o índice de performance.
--- =================================================================
-
-create index concurrently IF NOT exists 
-  profiles_id_idx on public.profiles (id);
