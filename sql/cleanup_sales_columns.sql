c53d8e1cc54c8c703a95fe52644b7e1b71a4115c
Daniel César <144414027+Dasksz@users.noreply.github.com>
2026-03-03 11:56:59 -0300

    Delete sql/cleanup_sales_columns.sql

--- sql/cleanup_sales_columns.sql
+++
@@ -1,7 +0,0 @@
--- Script de Limpeza: Remove a coluna 'qtvenda_embalagem_master' das tabelas de vendas
--- Esta informação passará a residir apenas na tabela dim_produtos
-
-ALTER TABLE IF EXISTS data_detailed DROP COLUMN IF EXISTS qtvenda_embalagem_master CASCADE;
-ALTER TABLE IF EXISTS data_history DROP COLUMN IF EXISTS qtvenda_embalagem_master CASCADE;
-
--- Fim do script
