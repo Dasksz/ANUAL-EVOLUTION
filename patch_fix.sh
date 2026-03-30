66d7151a1c69c4da8ab81761f94aef8fa60be8a1
Daniel César <144414027+Dasksz@users.noreply.github.com>
2026-03-30 08:34:53 -0300

    Delete patch_fix.sh

--- patch_fix.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-# Move ALTER FUNCTION to AFTER CREATE OR REPLACE FUNCTION
-grep -v "ALTER FUNCTION public.get_innovations_data" sql/full_system_v1.sql > temp_sql
-echo "ALTER FUNCTION public.get_innovations_data(p_filial text[], p_cidade text[], p_supervisor text[], p_vendedor text[], p_rede text[], p_tipovenda text[], p_categoria_inovacao text, p_ano text, p_mes text) SET search_path = public;" >> temp_sql
-mv temp_sql sql/full_system_v1.sql
