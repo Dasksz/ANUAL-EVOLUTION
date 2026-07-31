83f5e4e3c08e0160372ad4d3f187e9f00b887673
Daniel César <144414027+Dasksz@users.noreply.github.com>
2026-07-31 12:45:45 -0300

    Delete shadow_test_get_boxes_dashboard_data.sql

--- shadow_test_get_boxes_dashboard_data.sql
+++
@@ -1,9 +0,0 @@
-BEGIN;
-
-EXPLAIN ANALYZE
-SELECT count(*) FROM data_detailed WHERE EXTRACT(YEAR FROM dtped) = 2024;
-
-EXPLAIN ANALYZE
-SELECT count(*) FROM data_detailed WHERE dtped >= make_date(2024, 1, 1) AND dtped <= make_date(2024, 12, 31);
-
-ROLLBACK;
